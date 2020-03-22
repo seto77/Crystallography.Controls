3d2338792df52f3328c74afc41fd1d4a4cd51b9f
seto <seto77@gmail.com>
2020-03-23 03:34:29 +0900

    20200323

---
+++ Crystal/FormScatteringFactor.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Crystallography.Controls
+{
+    public partial class FormScatteringFactor : Form
+    {
+        public Crystal crystal;
+        public CrystalControl crystalControl;
+
+        public FormScatteringFactor()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalControl_CrystalChanged(Crystal crystal)
+        {
+            ChangeCrystal(crystal);
+        }
+
+        private void FormCrystallographicInformation_Load(object sender, EventArgs e)
+        {
+            crystalControl.CrystalChanged += new CrystalControl.MyEventHandler(crystalControl_CrystalChanged);
+            ChangeCrystal(crystal);
+        }
+
+        //åãèªÇïœçXÇ∑ÇÈ
+        public void ChangeCrystal(Crystal crystal)
+        {
+            this.crystal = crystal;
+            numericUpDownThresholdD.Minimum = (decimal)((crystal.A + crystal.B + crystal.C) / 20);
+            SetSortedPlanes();
+        }
+
+        private void numericUpDownThresholdD_ValueChanged(object sender, EventArgs e)
+        {
+            SetSortedPlanes();
+        }
+
+        private void SetSortedPlanes()
+        {
+            dataSet.DataTableScatteringFactor.Clear();
+            for (int i = 0; i < this.dataGridView2.Columns.Count; i++)
+                this.dataGridView2.Columns[i].Visible = true;
+
+            if (!checkBoxHideEquivalentPlane.Checked)
+                this.dataGridView2.Columns[3].Visible = false; //MultiÇè¡Ç∑
+            if (checkBoxHideProhibitedPlanes.Checked)
+                this.dataGridView2.Columns[12].Visible = false;
+
+            Crystal c = (Crystal)crystal.Clone();
+
+            c.SetVectorOfG((double)numericUpDownThresholdD.Value / 10, waveLengthControl1.WaveSource, false);
+
+            if (c.VectorOfG.Count == 0) return;
+
+            double max = c.VectorOfG.Max(g => g.RawIntensity);
+            for (int i = 0; i < c.VectorOfG.Count; i++)
+                c.VectorOfG[i].RelativeIntensity = c.VectorOfG[i].RawIntensity / max;
+
+            if (checkBoxBragBrentano.Checked)
+            {
+                max = double.NegativeInfinity;
+                for (int i = 0; i < c.VectorOfG.Count; i++)
+                {
+                    Vector3D g = c.VectorOfG[i];
+                    int multi = 0;
+                    double twoTheta = 2 * Math.Asin(g.Length* waveLengthControl1.WaveLength / 2);
+                    bool irreducible = SymmetryStatic.IsRootIndex(g.h, g.k, g.l, c.Symmetry, ref multi);
+                    if (irreducible && !double.IsNaN(twoTheta))
+                    {
+                        if (waveLengthControl1.WaveSource == WaveSource.Xray)
+                            c.VectorOfG[i].RawIntensity = g.F.Magnitude2() * multi / c.CellVolumeSqure * (1 + Math.Cos(twoTheta) * Math.Cos(twoTheta)) / Math.Sin(twoTheta) / Math.Sin(twoTheta / 2);
+                        else
+                            c.VectorOfG[i].RawIntensity = g.F.Magnitude2() * multi / c.CellVolumeSqure / Math.Sin(twoTheta) / Math.Sin(twoTheta / 2);
+
+                        max = Math.Max(max, c.VectorOfG[i].RawIntensity);
+                    }
+                }
+
+                for (int i = 0; i < c.VectorOfG.Count; i++)
+                    c.VectorOfG[i].RelativeIntensity = c.VectorOfG[i].RawIntensity / max;
+            }
+
+            var dataMember = bindingSourceScatteringFactor.DataMember;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            bindingSourceScatteringFactor.DataMember = "";
+            foreach (Vector3D g in c.VectorOfG)
+            {
+                int multi = 0;
+                bool irreducible = SymmetryStatic.IsRootIndex(g.h, g.k, g.l, c.Symmetry, ref multi);
+                if (!checkBoxHideEquivalentPlane.Checked || irreducible)
+                {
+                    var condition = c.Symmetry.CheckExtinctionRule(g.h, g.k, g.l);//SymmetryStatic.CheckExtinctionRule(g.h, g.k, g.l, c.Symmetry);
+                    if (!checkBoxHideProhibitedPlanes.Checked || condition.Length == 0)
+                    {
+                        var d = 1 / g.Length* 10;
+                        var twoTheta = 2 * Math.Asin(g.Length* waveLengthControl1.WaveLength / 2) / Math.PI * 180;
+                        if (double.IsNaN(twoTheta))
+                            twoTheta = double.PositiveInfinity;
+                       dataSet.DataTableScatteringFactor.Add(g.h, g.k, g.l, multi, d, twoTheta, g.F, g.RelativeIntensity, g.Extinction);
+                    }
+                }
+            }
+            bindingSourceScatteringFactor.DataMember = dataMember;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+
+
+        }
+
+        private void FormCrystallographicInformation_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var str = new StringBuilder();
+           
+            for (int i = 0; i < dataGridView2.Columns.Count; i++)
+                if (dataGridView2.Columns[i].Visible)
+                    str.Append(dataGridView2.Columns[i].HeaderText + "\t");
+            str.Append("\r\n");
+
+            for (int j = 0; j < dataGridView2.Rows.Count; j++)
+            {
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                    if (dataGridView2.Columns[i].Visible)
+                        str.Append($"{dataGridView2[i, j].Value}\t");
+                str.Append("\r\n");
+            }
+            Clipboard.SetDataObject(str.ToString());
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void checkBoxBragBrentano_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxBragBrentano.Checked)
+            {
+                checkBoxHideEquivalentPlane.Checked = true;
+                checkBoxHideProhibitedPlanes.Checked = true;
+                checkBoxHideEquivalentPlane.Enabled = false;
+                checkBoxHideProhibitedPlanes.Enabled = false;
+            }
+            else
+            {
+                checkBoxHideEquivalentPlane.Enabled = true;
+                checkBoxHideProhibitedPlanes.Enabled = true;
+            }
+
+            SetSortedPlanes();
+        }
+
+        private void waveLengthControl1_WavelengthChanged(object sender, EventArgs e)
+        {
+            SetSortedPlanes();
+        }
+    }
+}
---
+++ DataSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Numerics;
+
+namespace Crystallography.Controls
+{
+    public partial class DataSet
+    {
+        partial class DataTableScatteringFactorDataTable
+        {
+            public void Add(int h, int k, int l, int mult, double d, double twoTheta, Complex f, double relInt, string[] condition)
+            {
+                DataRow dr = this.NewDataTableScatteringFactorRow();
+                dr[this.HColumn] = h;
+                dr[this.KColumn] = k;
+                dr[this.LColumn] = l;
+                dr[this.MultiColumn] = mult;
+                dr[this.DColumn] = d;
+                dr[this.QColumn] = Math.PI * 2 / d;
+                dr[this.TwoThetaColumn] = twoTheta;
+                dr[this.F_realColumn] = Math.Abs(f.Real) > 1E-18 ? f.Real : 0;
+                dr[this.F_invColumn] = Math.Abs(f.Imaginary) > 10E-18 ? f.Imaginary : 0;
+                dr[this.FColumn] = f.Magnitude > 1E-18 ? f.Magnitude : 0;
+                dr[this.F2Column] = f.Magnitude * f.Magnitude > 1E-18 ? f.Magnitude * f.Magnitude : 0;
+                dr[RelIntColumn] = relInt * 100 > 1E-18 ? relInt * 100 : 0;
+
+                System.Text.StringBuilder str = new System.Text.StringBuilder();
+                for (int m = 0; m < condition.Length; m++)
+                    str.Append(m == 0 ? condition[m] : " & " + condition[m]);
+
+                dr[columnCondition] = str.ToString();
+
+                this.Rows.Add(dr);
+            }
+
+            public new void Clear()
+            {
+                this.Rows.Clear();
+            }
+        }
+    }
+}
